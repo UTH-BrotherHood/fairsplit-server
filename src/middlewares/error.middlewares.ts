c5bb97d88d36c76492a433b2da1ff549df521ca0
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-07 05:08:25 +0700

    Add default error handling middleware for Express, including structured logging and sensitive data omission in error responses.

---
+++ src/middlewares/error.middlewares.ts
@@ -0,0 +1,46 @@
+import { ErrorRequestHandler } from 'express'
+import { omit } from 'lodash'
+import httpStatusCode from '~/core/statusCodes'
+import { logger } from '~/loggers/my-logger.log'
+import { ErrorWithStatus } from '~/utils/error.utils'
+
+// Định nghĩa rõ kiểu là ErrorRequestHandler
+export const defaultErrorHandler: ErrorRequestHandler = (err, req, res) => {
+  const requestInfo = {
+    method: req.method,
+    originalUrl: req.originalUrl,
+    params: req.params,
+    body: omit(req.body, ['password', 'confirmPassword']), // Loại bỏ các field nhạy cảm
+    query: req.query,
+    userIP: req.ip || req.socket.remoteAddress
+  }
+
+  logger.error(
+    err.message,
+    'ErrorHandler', // Tên của service/context
+    Array.isArray(req.headers['x-request-id'])
+      ? req.headers['x-request-id'][0]
+      : req.headers['x-request-id'] || 'NO_REQUEST_ID', // requestId
+    {
+      errorName: err.name,
+      errorStack: err.stack,
+      status: err instanceof ErrorWithStatus ? err.status : httpStatusCode.INTERNAL_SERVER_ERROR,
+      requestInfo
+    }
+  )
+
+  if (err instanceof ErrorWithStatus) {
+    res.status(err.status).json(omit(err, 'status'))
+    return
+  }
+
+  Object.getOwnPropertyNames(err).forEach((key) => {
+    Object.defineProperty(err, key, { enumerable: true })
+  })
+
+  res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+    message: err.message,
+    errorInfo: omit(err, 'stack')
+  })
+  return
+}
