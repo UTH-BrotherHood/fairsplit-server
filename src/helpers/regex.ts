64c81ede689b426f01e2b5beff692182bd12ca2b
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-07 05:06:51 +0700

    Add regular expression constants for name and email validation in regex.ts

---
+++ src/helpers/regex.ts
@@ -0,0 +1,2 @@
+export const NAME_REGEXP = /^[\w'\-,.][^_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]\s]{2,}$/
+export const EMAIL_REGEXP = /^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([0-9a-zA-Z]+[-._])*[0-9a-zA-Z]+\.[a-zA-Z]{2,3}$/
