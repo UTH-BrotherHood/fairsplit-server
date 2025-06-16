b27264fd911e23566908bb3f3885baae2646bee2
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-16 23:39:21 +0700

    Enhance type safety in wrapRequestHandler by adding generic parameters for request and response types. This improves the flexibility and maintainability of the handler function in the Express application.

--- src/utils/wrapHandler.ts
+++ src/utils/wrapHandler.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
 
 // Sử dụng RequestHandler từ express
-export const wrapRequestHandler = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<any> | any
-): RequestHandler => {
-  return (req: Request, res: Response, next: NextFunction) => {
+export const wrapRequestHandler = <P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = any>(
+  fn: (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => Promise<any> | any
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
+  return (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => {
       console.error('Error caught in wrapRequestHandler:', err)
       next(err)
