42644fedda655bd665a57d0590dfbc4b5264b51a
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-16 23:07:17 +0700

    Refactor database services to streamline collection management by adding new collection getters for groups, transactions, notifications, settings, error logs, audit logs, categories, bills, and friend requests. Removed unused code and improved type safety in collection retrieval methods.

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,9 +1,18 @@
-import { Collection, Db, IndexDescription, MongoClient } from 'mongodb'
-import { IToken, TokenModel } from '~/models/schemas/token.schema'
-import { IUser, UserModel } from '~/models/schemas/user.schema'
+import { Collection, Db, MongoClient } from 'mongodb'
+import { IToken } from '~/models/schemas/token.schema'
+import { IUser } from '~/models/schemas/user.schema'
 import config from '~/config/env'
-import { IVerificationCode, VerificationCodeModel } from '~/models/schemas/verificationCode.schema'
-import { CategoryModel } from '~/models/schemas/category.schema'
+import { IVerificationCode } from '~/models/schemas/verificationCode.schema'
+import { IGroup } from '~/models/schemas/group.schema'
+import { ITransaction } from '~/models/schemas/transaction.schema'
+import { INotification } from '~/models/schemas/notification.schema'
+import { ISetting } from '~/models/schemas/setting.schema'
+import { IErrorLog } from '~/models/schemas/errorLog.schema'
+import { IAuditLog } from '~/models/schemas/auditLog.schema'
+import { IAdmin } from '~/models/schemas/admin.schema'
+import { ICategory } from '~/models/schemas/category.schema'
+import { IBill } from '~/models/schemas/bill.schema'
+import { IFriendRequest } from '~/models/schemas/friendRequest.schema'
 
 export class DatabaseServices {
   private client: MongoClient
@@ -14,9 +23,6 @@ export class DatabaseServices {
     if (!uri) {
       throw new Error('MongoDB URI is not provided in environment variables')
     }
-
-    // Add logging to debug
-    console.log('MongoDB URI:', uri)
 
     this.client = new MongoClient(uri, {
       maxPoolSize: 10,
@@ -32,8 +38,6 @@ export class DatabaseServices {
       await this.client.connect()
       await this.db.command({ ping: 1 })
       console.log('You successfully connected to MongoDB!')
-
-      // await this.initCollections()
     } catch (error) {
       console.log('Error connecting to the database', error)
       await this.client.close()
@@ -41,56 +45,66 @@ export class DatabaseServices {
     }
   }
 
-  // private async initCollections() {
-  //   const collections = await this.db.listCollections().toArray()
-  //   const collectionNames = collections.map((c) => c.name)
-
-  //   if (!collectionNames.includes(config.dbUserCollection)) {
-  //     await this.db.createCollection(config.dbUserCollection, {
-  //       validator: {
-  //         $jsonSchema: UserModel.jsonSchema
-  //       }
-  //     })
-  //     await this.users.createIndexes([...UserModel.indexes] as unknown as IndexDescription[])
-  //   }
-
-  //   if (!collectionNames.includes(config.dbTokenCollection)) {
-  //     await this.db.createCollection(config.dbTokenCollection, {
-  //       validator: {
-  //         $jsonSchema: TokenModel.jsonSchema
-  //       }
-  //     })
-  //     //   await this.tokens.createIndexes([...TokenModel.indexes] as unknown as IndexDescription[])
-  //   }
-
-  //   if (!collectionNames.includes(config.dbVerificationCodeCollection)) {
-  //     await this.db.createCollection(config.dbVerificationCodeCollection, {
-  //       validator: {
-  //         $jsonSchema: VerificationCodeModel.jsonSchema
-  //       }
-  //     })
-  //     //   await this.verificationCodes.createIndexes([...VerificationCodeModel.indexes] as unknown as IndexDescription[])
-  //   }
-
-  //   if (!collectionNames.includes(config.dbCategoryCollection)) {
-  //     await this.db.createCollection(config.dbCategoryCollection, {
-  //       validator: {
-  //         $jsonSchema: CategoryModel.jsonSchema
-  //       }
-  //     })
-  //   }
-  // }
+  async disconnect() {
+    try {
+      await this.client.close()
+      console.log('Disconnected from the database')
+    } catch (error) {
+      console.error('Error disconnecting from the database:', error)
+      throw error
+    }
+  }
 
   get users(): Collection<IUser> {
-    return this.db.collection(config.dbUserCollection)
+    return this.db.collection<IUser>(config.dbUserCollection)
   }
 
   get tokens(): Collection<IToken> {
-    return this.db.collection(config.dbTokenCollection)
+    return this.db.collection<IToken>(config.dbTokenCollection)
+  }
+
+  get admins(): Collection<IAdmin> {
+    return this.db.collection<IAdmin>(config.dbAdminCollection)
   }
 
   get verificationCodes(): Collection<IVerificationCode> {
-    return this.db.collection(config.dbVerificationCodeCollection)
+    return this.db.collection<IVerificationCode>(config.dbVerificationCodeCollection)
+  }
+
+  get groups(): Collection<IGroup> {
+    return this.db.collection<IGroup>(config.dbGroupCollection)
+  }
+
+  get transactions(): Collection<ITransaction> {
+    return this.db.collection<ITransaction>(config.dbTransactionCollection)
+  }
+
+  get notifications(): Collection<INotification> {
+    return this.db.collection<INotification>(config.dbNotificationCollection)
+  }
+
+  get settings(): Collection<ISetting> {
+    return this.db.collection<ISetting>(config.dbSettingCollection)
+  }
+
+  get errorLogs(): Collection<IErrorLog> {
+    return this.db.collection<IErrorLog>(config.dbErrorLogCollection)
+  }
+
+  get auditLogs(): Collection<IAuditLog> {
+    return this.db.collection<IAuditLog>(config.dbAuditLogCollection)
+  }
+
+  get categories(): Collection<ICategory> {
+    return this.db.collection<ICategory>(config.dbCategoryCollection)
+  }
+
+  get bills(): Collection<IBill> {
+    return this.db.collection<IBill>(config.dbBillCollection)
+  }
+
+  get friendRequests(): Collection<IFriendRequest> {
+    return this.db.collection<IFriendRequest>(config.dbFriendRequestCollection)
   }
 }
 
