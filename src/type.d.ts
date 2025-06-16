717c9cf3a768011b841e3c495ca0853363928987
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-16 22:53:02 +0700

    Update type definitions in type.d.ts to include decodedForgotPasswordToken. This change enhances the request interface by adding support for handling forgot password token data.

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,11 +1,12 @@
 import { IUser } from './models/schemas/user.schema'
-import { TokenPayload } from './types/auth.types'
+import { TokenPayload } from './models/requests/user.requests'
 
 declare global {
   namespace Express {
     interface Request {
       decodedRefreshToken?: TokenPayload
       decodedAuthorization?: TokenPayload
+      decodedForgotPasswordToken?: TokenPayload
       user?: IUser
       fileUrl?: string
       fileUrls?: string[]
