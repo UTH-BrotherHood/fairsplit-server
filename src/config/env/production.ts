554299647d80ed870c947d161424ed5ab8ddbc4b
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-07-11 04:44:28 +0700

    Remove Google OAuth configuration from development and production environment files to streamline environment setup.

--- src/config/env/production.ts
+++ src/config/env/production.ts
@@ -45,10 +45,6 @@ export const envConfig = {
 
   // google oauth20
   googleClientId: process.env.GOOGLE_CLIENT_ID as string,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  googleCallbackURLDev: process.env.GOOGLE_CALLBACK_URL_DEV as string,
-  googleCallbackURLProd: process.env.GOOGLE_CALLBACK_URL_PROD as string,
-  googleRedirectClientUrl: process.env.GOOGLE_REDIRECT_CLIENT_URL as string,
 
   // Redis
   redisHost: process.env.REDIS_HOST as string,
